627f20f17c53ac41036826b62a0e5a958160e53a
Davy Stevenson <davy.stevenson@gmail.com>
2014-08-07 20:19:04 -0700

    'Fix' tests for realz

--- test/benchmark/test_bigo.rb
+++ test/benchmark/test_bigo.rb
@@ -1,7 +1,7 @@
 require "minitest/autorun"
 require "benchmark/bigo"
 
-class TestBenchmark::TestBigo < MiniTest::Test
+class TestBenchmarkBigo < MiniTest::Test
   def test_sanity
 #    flunk "write tests or I will kneecap you"
   end
