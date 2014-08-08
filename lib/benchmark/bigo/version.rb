7ac0b0a18d1cc7aa902de0e4ae583ceb588ec61f
Davy Stevenson <davy.stevenson@gmail.com>
2014-08-08 16:46:07 -0700

    Remove DATE from Version

--- lib/benchmark/bigo/version.rb
+++ lib/benchmark/bigo/version.rb
@@ -2,12 +2,11 @@ module Benchmark
   module BigO
     class Version
       MAJOR = 0
-      MINOR = 0
-      PATCH = 1
-      DATE  = 20140808093808
+      MINOR = 1
+      PATCH = 0
 
       def self.to_s
-        [MAJOR, MINOR, PATCH, DATE].compact.join('.')
+        [MAJOR, MINOR, PATCH].compact.join('.')
       end
     end
 
