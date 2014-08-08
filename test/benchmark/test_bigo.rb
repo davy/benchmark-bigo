d422c5e3630eb83beedf5cc51793944ecfd339f2
Davy Stevenson <davy.stevenson@gmail.com>
2014-08-08 13:34:11 -0700

    Moar tests

--- test/benchmark/test_bigo.rb
+++ test/benchmark/test_bigo.rb
@@ -13,15 +13,12 @@ class TestBenchmarkBigo < MiniTest::Test
 
   def test_bigo
     report = Benchmark.bigo do |x|
-      x.config(:time => 1, :warmup => 1)
-      x.generate(:array)
-      x.increments = 2
+      x.config(:time => 1, :warmup => 1, :increments => 2)
+      x.generate :array
 
       x.report("#at") {|array, size| array.at rand(size) }
       x.report("#index") {|array, size| array.index rand(size) }
       x.report("sleep") { |a,b| sleep(0.25) }
-
-      x.compare!
     end
 
     assert_equal 3, report.entries.length
@@ -36,6 +33,7 @@ class TestBenchmarkBigo < MiniTest::Test
 
     assert_equal 2, at_rep.size
     assert_equal 2, index_rep.size
+    assert_equal 2, sleep_rep.size
 
     assert_equal "#at 100", at_rep[0].label
     assert_equal "#at 200", at_rep[1].label
@@ -49,5 +47,103 @@ class TestBenchmarkBigo < MiniTest::Test
     assert_equal 4, sleep_rep[0].iterations
     assert_in_delta 4.0, sleep_rep[0].ips, 0.2
 
+  end
+
+  def test_bigo_alternate_config
+    report = Benchmark.bigo do |x|
+      x.time = 1
+      x.warmup = 1
+      x.increments = 2
+      x.generate(:array)
+      x.report("sleep") { |a,b| sleep(0.25) }
+    end
+
+    rep = report.entries["sleep"]
+    assert_equal 2, rep.size
+
+    assert_equal "sleep 100", rep[0].label
+    assert_equal 4, rep[0].iterations
+    assert_in_delta 4.0, rep[0].ips, 0.2
+  end
+
+  def test_bigo_defaults
+    report = Benchmark.bigo do |x|
+      x.config(:time => 1, :warmup => 1)
+      x.generate :array
+      x.report("sleep") { |a,b| sleep(0.25) }
+    end
+
+    assert_equal 1, report.entries.keys.length
+    rep = report.entries["sleep"]
+
+    assert_equal 5, rep.size
+
+    assert_equal "sleep 100", rep[0].label
+    assert_equal "sleep 200", rep[1].label
+    assert_equal "sleep 300", rep[2].label
+    assert_equal "sleep 400", rep[3].label
+    assert_equal "sleep 500", rep[4].label
+
+    assert_equal 4, rep[0].iterations
+    assert_in_delta 4.0, rep[0].ips, 0.2
+  end
+
+  def test_bigo_logarithmic
+    report = Benchmark.bigo do |x|
+      x.config(:time => 1, :warmup => 1, :increments => 2)
+      x.generate :array
+      x.logarithmic
+      x.report("sleep") { |a,b| sleep(0.25) }
+    end
+
+    rep = report.entries["sleep"]
+
+    assert_equal "sleep 1", rep[0].label
+    assert_equal "sleep 10", rep[1].label
+    assert_equal 4, rep[0].iterations
+    assert_in_delta 4.0, rep[0].ips, 0.2
+  end
+
+  def test_bigo_increments_config
+    report = Benchmark.bigo do |x|
+      x.config(:time => 1, :warmup => 1, :increments => 3)
+      x.generate :array
+
+      x.report("#at") {|array, size| array.at rand(size) }
+    end
+
+    assert_equal 1, report.entries.length
+
+    assert report.entries.keys.include?("#at")
+
+    at_rep = report.entries["#at"]
+
+    assert_equal 3, at_rep.size
+
+    assert_equal "#at 100", at_rep[0].label
+    assert_equal "#at 200", at_rep[1].label
+    assert_equal "#at 300", at_rep[2].label
+  end
+
+  def test_bigo_increments_setter
+    report = Benchmark.bigo do |x|
+      x.config(:time => 1, :warmup => 1)
+      x.increments = 3
+      x.generate :array
+
+      x.report("#at") {|array, size| array.at rand(size) }
+    end
+
+    assert_equal 1, report.entries.length
+
+    assert report.entries.keys.include?("#at")
+
+    at_rep = report.entries["#at"]
+
+    assert_equal 3, at_rep.size
+
+    assert_equal "#at 100", at_rep[0].label
+    assert_equal "#at 200", at_rep[1].label
+    assert_equal "#at 300", at_rep[2].label
   end
 end
